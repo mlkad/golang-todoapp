de787e69885696cf5eeadfe2ecb301f5fff43878
Malika <dusmagambetovaaa@gmail.com>
2026-07-14 15:42:02 +0500

    feat: add statistics feature

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -13,6 +13,9 @@ import (
 	core_pgx_pool "github.com/mlkad/golang-todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/mlkad/golang-todoapp/internal/core/transport/http/middleware"
 	core_http_server "github.com/mlkad/golang-todoapp/internal/core/transport/http/server"
+	statistics_postgres_repository "github.com/mlkad/golang-todoapp/internal/features/statistics/repository/postgres"
+	statistics_service "github.com/mlkad/golang-todoapp/internal/features/statistics/service"
+	statistics_transport_http "github.com/mlkad/golang-todoapp/internal/features/statistics/transport/http"
 	tasks_postgres_repository "github.com/mlkad/golang-todoapp/internal/features/tasks/repository/postgres"
 	tasks_service "github.com/mlkad/golang-todoapp/internal/features/tasks/service"
 	tasks_transport_http "github.com/mlkad/golang-todoapp/internal/features/tasks/transport/http"
@@ -62,6 +65,11 @@ func main() {
 	tasksService := tasks_service.NewTasksService(tasksRepository)
 	tasksTransportHTTP := tasks_transport_http.NewTasksHTTPHandler(tasksService)
 
+	logger.Debug("initializing feature", zap.String("feature", "statistics"))
+	statisticsRepository := statistics_postgres_repository.NewStatisticsRepository(pool)
+	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
+	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
+
 	logger.Debug("initializing HTTP server")
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -74,6 +82,7 @@ func main() {
 	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
 	apiVersionRouter.RegisterRoutes(tasksTransportHTTP.Routes()...)
+	apiVersionRouter.RegisterRoutes(statisticsTransportHTTP.Routes()...)
 
 	httpServer.RegisterAPIRouters(apiVersionRouter)
 
